2f1d6e62d4b7d5c3f993b803defbd86273115f47
K-001 <TheLearningBuddha8@gmail.com>
2022-07-29 14:15:30 +0530

    commit calc2 in branchX

---
+++ src/oops/Calculation2.java
@@ -0,0 +1,17 @@
+package oops;
+
+public class Calculation2 {
+	int z;
+	
+	public void addition(int x, int y) {
+		z =x+y;
+		System.out.println("sum : " + z);
+	}
+	
+	public void Substraction(int x, int y) {
+		z = x-y;
+		System.out.println("diff : "+ z);
+	}
+	
+}
+
