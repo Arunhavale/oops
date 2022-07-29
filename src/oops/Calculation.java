c02ee026ca539ada6decad2e78c39a75d1e7e8eb
K-001 <TheLearningBuddha8@gmail.com>
2022-07-29 14:36:30 +0530

    new changes done in calc2

--- src/oops/Calculation.java
+++ src/oops/Calculation.java
@@ -6,10 +6,12 @@ public class Calculation {
 	public void addition(int x, int y) {
 		z =x+y;
 		System.out.println("sum : " + z);
+		System.out.println("nothing channges");
 	}
 	
 	public void Substraction(int x, int y) {
 		z = x-y;
+		z=x+y;
 		System.out.println("diff : "+ z);
 	}
 	
